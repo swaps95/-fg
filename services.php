29d1b9cf2f3b32b31c52f15f57fb46afaf49eb85
swaps95 <chowkek@gmail.com>
2017-04-20 14:25:27 +0530

    comm

---
+++ services.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.5.3, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.5.3, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/resolutions-upto-2400-dpi-hd-quality-ae9wa3-425x128-14.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/mobirise/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section class="mbr-navbar mbr-navbar--freeze mbr-navbar--absolute mbr-navbar--sticky mbr-navbar--auto-collapse" id="ext_menu-0">
+    <div class="mbr-navbar__section mbr-section">
+        <div class="mbr-section__container container">
+            <div class="mbr-navbar__container">
+                <div class="mbr-navbar__column mbr-navbar__column--s mbr-navbar__brand">
+                    <span class="mbr-navbar__brand-link mbr-brand mbr-brand--inline">
+                        <span class="mbr-brand__logo"><a href="https://mobirise.com"><img src="assets/images/resolutions-upto-2400-dpi-hd-quality-ae9wa3-425x128-14.png" class="mbr-navbar__brand-img mbr-brand__img" alt="Mobirise"></a></span>
+                        
+                    </span>
+                </div>
+                <div class="mbr-navbar__hamburger mbr-hamburger"><span class="mbr-hamburger__line"></span></div>
+                <div class="mbr-navbar__column mbr-navbar__menu">
+                    <nav class="mbr-navbar__menu-box mbr-navbar__menu-box--inline-right">
+                        <div class="mbr-navbar__column">
+                            <ul class="mbr-navbar__items mbr-navbar__items--right float-left mbr-buttons mbr-buttons--freeze mbr-buttons--right btn-decorator mbr-buttons--active mbr-buttons--only-links"> <li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="https://mobirise.com">HOME</a></li><li class="mbr-navbar__item"><a class="mbr-buttons__link btn text-white" href="https://mobirise.com">CONTACT</a></li></ul>                            
+                            
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="engine"><a rel="external" href="https://mobirise.com">free wysiwyg site creator</a></section><section class="mbr-section mbr-section--relative mbr-after-navbar" id="msg-box4-0" style="background-color: rgb(255, 255, 255);">
+    
+    <div class="mbr-section__container mbr-section__container--isolated container" style="padding-top: 0px; padding-bottom: 31px;">
+        <div class="row">
+            <div class="mbr-box mbr-box--fixed mbr-box--adapted">
+                <div class="mbr-box__magnet mbr-box__magnet--top-right mbr-section__left col-sm-6">
+                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><img src="assets/images/web-design-1-1400x892-3.jpg" class="mbr-figure__img"></figure>
+                </div>
+                <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__right">
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
+                            <h3 class="mbr-header__text">WEB DEVELOPEMENT</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><p>Business is done with people not robots and interaction is what one needs to have with their customers. We develop your web presence which communicates your business mantra in its first glimpse. We make sure it reflects your work at its best to make your visitor turn into an admirer.</p></div>
+                    </div>
+                    <div class="mbr-section__container">
+                        <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"><a class="mbr-buttons__btn btn btn-lg btn-success" href="web_development.php">READ MORE</a> <a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">REQUEST PRICE</a></div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section--relative" id="msg-box4-1" style="background-color: rgb(255, 255, 255);">
+    
+    <div class="mbr-section__container mbr-section__container--isolated container" style="padding-top: 124px; padding-bottom: 31px;">
+        <div class="row">
+            <div class="mbr-box mbr-box--fixed mbr-box--adapted">
+                
+                <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__left">
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
+                            <h3 class="mbr-header__text">DESIGN</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><p>Our team works on innovative solutions which are appealing both functionally and visually. Ardent designers work on every aspect of your business so your customers connect with your ideology and understand your product better.</p></div>
+                    </div>
+                    <div class="mbr-section__container">
+                        <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"><a class="mbr-buttons__btn btn btn-lg btn-success" href="design.php">READ MORE</a> <a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">REQUEST PRICE</a></div>
+                    </div>
+                </div>
+                <div class="mbr-box__magnet mbr-box__magnet--top-left mbr-section__right col-sm-6">
+                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><img src="assets/images/3-gouden-regels-bij-het-vernieuwen-van-een-website-1400x677-24.png" class="mbr-figure__img"></figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section--relative" id="msg-box4-2" style="background-color: rgb(255, 255, 255);">
+    
+    <div class="mbr-section__container mbr-section__container--isolated container" style="padding-top: 0px; padding-bottom: 31px;">
+        <div class="row">
+            <div class="mbr-box mbr-box--fixed mbr-box--adapted">
+                <div class="mbr-box__magnet mbr-box__magnet--top-right mbr-section__left col-sm-6">
+                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><img src="assets/images/internet-of-things-are-your-user-experience-design-skills-up-to-speed-1400x979-82.jpg" class="mbr-figure__img"></figure>
+                </div>
+                <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__right">
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
+                            <h3 class="mbr-header__text">THEMING AND INTEGRATION</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface.</p></div>
+                    </div>
+                    <div class="mbr-section__container">
+                        <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"><a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">READ MORE</a> <a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">REQUEST PRICE</a></div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section--relative" id="msg-box4-3" style="background-color: rgb(255, 255, 255);">
+    
+    <div class="mbr-section__container mbr-section__container--isolated container" style="padding-top: 0px; padding-bottom: 0px;">
+        <div class="row">
+            <div class="mbr-box mbr-box--fixed mbr-box--adapted">
+                
+                <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__left">
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
+                            <h3 class="mbr-header__text">E-Commerce</h3>
+                            
+                        </div>
+                    </div>
+                    <div class="mbr-section__container mbr-section__container--middle">
+                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><p>A combination of design and performance which highlights your range of products is what we deliver. An E-commerce store has different requirements of its own, then be it ease of navigation, product presentation or fast and reliable shopping cart. We develop most optimised solutions in ecommerce, so your website offers a pleasant experience not just a collection of webpages.</p></div>
+                    </div>
+                    <div class="mbr-section__container">
+                        <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"><a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">READ MORE</a> <a class="mbr-buttons__btn btn btn-lg btn-success" href="https://mobirise.com">REQUEST PRICE</a></div>
+                    </div>
+                </div>
+                <div class="mbr-box__magnet mbr-box__magnet--top-left mbr-section__right col-sm-6">
+                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><img src="assets/images/6-important-factors-in-building-ecommerce-websites-1400x700-70.jpg" class="mbr-figure__img"></figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="form1-0" style="background-color: rgb(239, 239, 239);">
+    
+    <div class="mbr-section__container mbr-section__container--std-padding container" style="padding-top: 93px; padding-bottom: 93px;">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <div class="mbr-header mbr-header--center mbr-header--std-padding">
+                            <h2 class="mbr-header__text">CONTACT FORM</h2>
+                        </div>
+                        
+                        <form action="contact.php" method="post">
+                            
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="name" required="" placeholder="Name*">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" required="" placeholder="Email*">
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control" name="phone" placeholder="Phone">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="7" placeholder="Message"></textarea>
+                            </div>
+                            <div class="mbr-buttons mbr-buttons--right"><button type="submit" class="mbr-buttons__btn btn btn-lg btn-danger">CONTACT US</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="contacts2-0" style="background-color: rgb(255, 255, 255);">
+    
+    <div class="mbr-section__container container">
+        <div class="mbr-contacts mbr-contacts--wysiwyg row" style="padding-top: 45px; padding-bottom: 45px;">
+            <div class="col-sm-6">
+                <figure class="mbr-figure mbr-figure--wysiwyg mbr-figure--full-width mbr-figure--no-bg">
+                    <div class="mbr-figure__map mbr-figure__map--short"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:ChIJtcaxrqlZwokRfwmmibzPsTU" allowfullscreen=""></iframe></div>
+                </figure>
+            </div>
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-5 col-sm-offset-1">
+                        <p class="mbr-contacts__text"><strong>ADDRESS</strong><br>12, Varad Enclave, <br>Near Ghokhle Park Society, Bhangarwadi, Lonavala, Pune, Maharashtra<br>Pin- 410401<br><br>
+<strong>CONTACTS</strong><br>
+Email:<br>info@inceptumtechnologies.co.in<br>
+Phone: <br>+91-9921589715<br><br></p>
+                    </div>
+                    <div class="col-sm-6"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
+  <!--[if lte IE 9]>
+    <script src="assets/jquery-placeholder/jquery.placeholder.min.js"></script>
+  <![endif]-->
+  <script src="assets/mobirise/js/script.js"></script>
+  
+  
+</body>
+</html>
